b1454e0d95befb76d71858cac684db581ae2eb3c
Thorhallur O <thorhallur30102004@gmail.com>
2022-07-31 17:10:46 +0000

    Updated utils.ts file

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,15 @@
 import { fileURLToPath } from 'url'
-import * as path from 'path'
+import * as _path from 'path'
 
-export const __dirname:string = path.dirname(fileURLToPath(import.meta.url + '\\..'))
+class Util {
+    public readonly _filterArray:any[] = ['', undefined, null, NaN, Infinity, []]
+    public readonly __filterArray:any[] = ['', undefined, null, NaN, Infinity, [], [this._filterArray]]
+    public readonly __dirname:string = _path.dirname(fileURLToPath(import.meta.url + '\\..'))
+    public getPath(path:string, extension:string='\\'):string { return (this.__dirname + `\\${path}${extension}`).replace('/', '\\') }
+    public compareStr(x:string, y:string) { return x.trim().toLowerCase() == y.trim().toLowerCase() }
+    public filterArray(array:string[]):string[] { return array.filter((item) => !this.__filterArray.includes(item) ) }
+    public splitLines(text:string):string[] { return this.filterArray(text.split(/\r?\n/)) }
+    public splitLinesNoFilter(text:string):string[] { return text.split(/\r?\n/) }
+}
 
-export function compareStr(x:string, y:string) { return x.trim().toLowerCase() == y.trim().toLowerCase() }
-export function splitLines(text:string):string[] { return text.split(/\r?\n/) }
+export default new Util()
